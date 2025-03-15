776dbab367dff39af18d94d7b5c970dd090e68dd
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-03-15 14:19:27 +0900

    추가 | 주사위

---
+++ Assets/00.Main/01.Script/Dice.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public int diceValue; // 주사위 값 (1~6)
+
+    public void SetValue(int value)
+    {
+        diceValue = value;
+    }
+
+    public IEnumerator RollDiceAnimation()
+    {
+        float duration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.Rotate(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        SetDiceFace(diceValue);
+    }
+    void SetDiceFace(int value)
+    {
+        // **회전 초기화**
+        transform.rotation = Quaternion.identity;
+
+        // **주사위 값에 따라 회전**
+        switch (value)
+        {
+            case 1: transform.Rotate(-90, 0, 0); break;   // 1번 면 (윗면)
+            case 2: transform.Rotate(0, 0, 0); break;    // 2번 면 (정면)
+            case 3: transform.Rotate(0, 0, -90); break;  // 3번 면 (오른쪽)
+            case 4: transform.Rotate(0, 0, 90); break;   // 4번 면 (왼쪽)
+            case 5: transform.Rotate(180, 0, 0); break;  // **5번 면 (바닥)**
+            case 6: transform.Rotate(90, 0, 0); break;   // **6번 면 (뒷면)**
+        }
+
+        Debug.DrawRay(transform.position, transform.up * 2, Color.red, 2f); // 디버그용
+    }
+
+}
--- Assets/00.Main/01.Script/Manager/DiceManager.cs
+++ Assets/00.Main/01.Script/Manager/DiceManager.cs
@@ -1,34 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 
 public class DiceManager : MonoBehaviour
 {
     public static DiceManager instance;
 
+    public Dice dice1;
+    public Dice dice2;
+
+    public int diceResult;
+
+    [SerializeField] GameObject dicePanel;
+    public TMP_Text diceResultText;
     private void Awake()
     {
         instance = this;
     }
-    public int diceResult;
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             RollDice();
+            StartCoroutine(ResultTextCor());
         }
+
+        diceResultText.text = diceResult.ToString();
     }
 
     void RollDice()
     {
-        int dice1 = Random.Range(1, 7); // 주사위 1: 1~6 사이의 랜덤 값
-        int dice2 = Random.Range(1, 7); // 주사위 2: 1~6 사이의 랜덤 값
+        int diceValue1 = Random.Range(1, 7);
+        int diceValue2 = Random.Range(1, 7);
 
-        int total = dice1 + dice2;
-        diceResult = total;
+        diceResult = diceValue1 + diceValue2;
 
-        Debug.Log($"주사위1: {dice1}, 주사위2: {dice2}, 합계: {total}");
+        dice1.SetValue(diceValue1);
+        dice2.SetValue(diceValue2);
+
+        StartCoroutine(dice1.RollDiceAnimation());
+        StartCoroutine(dice2.RollDiceAnimation());
+
+        Debug.Log($"주사위1: {diceValue1}, 주사위2: {diceValue2}, 합계: {diceResult}");
+    }
+
+    IEnumerator ResultTextCor()
+    {
+        dicePanel.SetActive(true);
+        diceResultText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        diceResultText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        dicePanel.SetActive(false);
 
     }
 }
