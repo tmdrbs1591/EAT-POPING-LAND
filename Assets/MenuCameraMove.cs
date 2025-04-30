5fd810eb1d500fb2214efa2cfca8e5c8ba1d1399
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-04-30 22:33:19 +0900

    추가 | 플레이어 인포 ui

--- Assets/MenuCameraMove.cs
+++ Assets/MenuCameraMove.cs
@@ -2,57 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class MenuCameraMove : MonoBehaviour
 {
-    [SerializeField] private Transform charSelectCameraPos; // 선택된 위치
-    private Vector3 originalPosition; // 원래 위치
-    private Quaternion originalRotation; // 원래 회전 값
+    [SerializeField] private Transform charSelectCameraPos;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
 
     [SerializeField] private GameObject charSelectBtn;
-    [SerializeField] private GameObject charSelectPanel;
+    [SerializeField] private RectTransform charSelectPanel;
+    [SerializeField] private RectTransform charImfoPanel;
 
-
-
+    private Vector2 charSelectPanelOriginalPos;
+    private Vector2 charImfoPanelOriginalPos;
 
     void Start()
     {
-        // 원래 위치와 회전 값을 저장
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+
+        charSelectPanelOriginalPos = charSelectPanel.anchoredPosition;
+        charImfoPanelOriginalPos = charImfoPanel.anchoredPosition;
+
+        // 처음에 패널 화면 밖으로
+        charSelectPanel.anchoredPosition = charSelectPanelOriginalPos + new Vector2(0, -Screen.height);
+        charImfoPanel.anchoredPosition = charImfoPanelOriginalPos + new Vector2(-Screen.width, 0);
+
+        charSelectPanel.gameObject.SetActive(false);
+        charImfoPanel.gameObject.SetActive(false);
     }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             MoveToOriginalPos();
         }
     }
 
-    // 카메라를 선택된 위치로 자연스럽게 이동시키는 함수
     public void MoveToSelectPos()
     {
-        AudioManager.instance.PlaySound(transform.position, 4, Random.Range(1f, 1f), 1f);
-        // DOTween을 이용해서 카메라 이동
-        transform.DOMove(charSelectCameraPos.position, 0.5f).SetEase(Ease.InOutQuad);
-        transform.DORotate(charSelectCameraPos.rotation.eulerAngles, 1f).SetEase(Ease.InOutQuad);
+        AudioManager.instance.PlaySound(transform.position, 4, 1f, 1f);
+
+        transform.DOMove(charSelectCameraPos.position, 0.3f).SetEase(Ease.InOutQuad);
+        transform.DORotate(charSelectCameraPos.rotation.eulerAngles, 0.3f).SetEase(Ease.InOutQuad);
+
         charSelectBtn.SetActive(false);
-        charSelectPanel.SetActive(true);
+
+        // UI 슬라이드 인
+        charSelectPanel.gameObject.SetActive(true);
+        charSelectPanel.DOAnchorPos(charSelectPanelOriginalPos, 0.3f).SetEase(Ease.InOutQuad);
+
+        charImfoPanel.gameObject.SetActive(true);
+        charImfoPanel.DOAnchorPos(charImfoPanelOriginalPos, 0.3f).SetEase(Ease.InOutQuad);
 
         CharacterManager.instance.isCharSelect = true;
-
     }
 
-    // 원래 위치로 카메라를 자연스럽게 이동시키는 함수
     public void MoveToOriginalPos()
     {
-        // DOTween을 이용해서 카메라 이동
-        transform.DOMove(originalPosition, 0.5f).SetEase(Ease.InOutQuad);
-        transform.DORotate(originalRotation.eulerAngles, 1f).SetEase(Ease.InOutQuad);
+        transform.DOMove(originalPosition, 0.3f).SetEase(Ease.InOutQuad);
+        transform.DORotate(originalRotation.eulerAngles, 0.3f).SetEase(Ease.InOutQuad);
+
         charSelectBtn.SetActive(true);
-        charSelectPanel.SetActive(false);
+
+        // UI 슬라이드 아웃
+        charSelectPanel.DOAnchorPos(charSelectPanelOriginalPos + new Vector2(0, -Screen.height), 0.3f)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() => charSelectPanel.gameObject.SetActive(false));
+
+        charImfoPanel.DOAnchorPos(charImfoPanelOriginalPos + new Vector2(-Screen.width, 0), 0.3f)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() => charImfoPanel.gameObject.SetActive(false));
 
         CharacterManager.instance.isCharSelect = false;
-
     }
 }
