6472f4ae060fabd33b704e8bf8f1ffa756d96c17
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-05-03 21:05:10 +0900

    추가 | 뒤끝 버전 낮춤

---
+++ Assets/00.Main/01.Script/Backend/BackendManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        BackendSetup();
+    }
+
+    private void BackendSetup()
+    {
+        var bro = Backend.Initialize(true);
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log($"초기화 성공 : {bro}");
+
+        }
+        else
+        {
+            Debug.Log($"초기화 실패 : {bro}" );
+        }
+    }
+}
