67b2b8938d7d6de1500981ac2d7c4c1454152516
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-05-04 13:10:19 +0900

    추가 | 로그인 기능 완성

--- Assets/00.Main/01.Script/Backend/RegisterAccount.cs
+++ Assets/00.Main/01.Script/Backend/RegisterAccount.cs
@@ -1,94 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using BackEnd;
-using Unity.VisualScripting;
 
 public class RegisterAccount : LoginBase
 {
     [SerializeField]
-    private Image imageID;
+    private Image imageID;              // ID 필드 색상 변경
     [SerializeField]
-    private TMP_InputField inputFieldID;
+    private TMP_InputField inputFieldID;            // ID 필드 텍스트 정보 추출
     [SerializeField]
-    private Image imagePW;
+    private Image imagePW;              // PW 필드 색상 변경
     [SerializeField]
-    private TMP_InputField inputFieldPW;
+    private TMP_InputField inputFieldPW;            // PW 필드 텍스트 정보 추출
     [SerializeField]
-    private Image imageConfirmPW;
+    private Image imageConfirmPW;           // Confirm PW 필드 색상 변경
     [SerializeField]
-    private TMP_InputField inputFieldConfirmPW;
+    private TMP_InputField inputFieldConfirmPW; // Confirm PW 필드 텍스트 정보 추출
     [SerializeField]
-    private Image imageEmail;
+    private Image imageEmail;               // E-mail 필드 색상 변경
     [SerializeField]
-    private TMP_InputField inputFieldEmail;
+    private TMP_InputField inputFieldEmail;     // E-mail 필드 텍스트 정보 추출
 
     [SerializeField]
-    private Button btnRegisterAccount;
+    private Button btnRegisterAccount;      // "계정 생성" 버튼 (상호작용 가능/불가능)
 
     /// <summary>
-    ///   계정생성 버튼을 눌렀을때 호출
+    /// "계정 생성" 버튼을 눌렀을 때 호출
     /// </summary>
     public void OnClickRegisterAccount()
     {
-        ResetUI(imageID,imagePW,imageConfirmPW,imageEmail);
+        // 매개변수로 입력한 InputField UI의 색상과 Message 내용 초기화
+        ResetUI(imageID, imagePW, imageConfirmPW, imageEmail);
 
+        // 필드 값이 비어있는지 체크
         if (IsFieldDataEmpty(imageID, inputFieldID.text, "아이디")) return;
         if (IsFieldDataEmpty(imagePW, inputFieldPW.text, "비밀번호")) return;
         if (IsFieldDataEmpty(imageConfirmPW, inputFieldConfirmPW.text, "비밀번호 확인")) return;
         if (IsFieldDataEmpty(imageEmail, inputFieldEmail.text, "메일 주소")) return;
 
-        //비밀번호와 비밀번호 확인 내용이다를떄
-        if (inputFieldPW.text != inputFieldConfirmPW.text)
+        // 비밀번호와 비밀번호 확인의 내용이 다를 때
+        if (!inputFieldPW.text.Equals(inputFieldConfirmPW.text))
         {
-            GuideForIncorretlyEnterData(imageConfirmPW, "비밀번호가 일치하지 않습니다");
+            GuideForIncorretlyEnterData(imageConfirmPW, "비밀번호가 일치하지 않습니다.");
             return;
         }
 
-
+        // 메일 형식 검사
         if (!inputFieldEmail.text.Contains("@"))
         {
-            GuideForIncorretlyEnterData(imageEmail, "메일 형식이 잘못되었습니다. (ex. address@xx.xx)");
+            GuideForIncorretlyEnterData(imageEmail, "메일 형식이 잘못되었습니다.(ex. address@xx.xx)");
             return;
         }
 
+        // "계정 생성" 버튼의 상호작용 비활성화
         btnRegisterAccount.interactable = false;
-        SetMessage("계정 생성중입니다..");
+        SetMessage("계정 생성중입니다.");
 
+        // 뒤끝 서버 계정 생성 시도
         CustomSignUp();
-
     }
 
+    /// <summary>
+    /// 계정 생성 시도 후 서버로부터 전달받은 message를 기반으로 로직 처리
+    /// </summary>
     private void CustomSignUp()
     {
         Backend.BMember.CustomSignUp(inputFieldID.text, inputFieldPW.text, callback =>
         {
-
+            // "계정 생성" 버튼 상호작용 활성화
             btnRegisterAccount.interactable = true;
 
+            // 계정 생성 성공
             if (callback.IsSuccess())
             {
-                SetMessage($"계정 생성 성공. {inputFieldID.text}님 환영합니다.");
+                // E-mail 정보 업데이트
+                Backend.BMember.UpdateCustomEmail(inputFieldEmail.text, callback =>
+                {
+                    if (callback.IsSuccess())
+                    {
+                        SetMessage($"계정 생성 성공. {inputFieldID.text}님 환영합니다.");
+
+                    }
+                });
             }
+            // 계정 생성 실패
             else
             {
                 string message = string.Empty;
 
                 switch (int.Parse(callback.GetStatusCode()))
                 {
-
-                    case 409:
-                        message = "이미 존재하는 아이디입니다";
+                    case 409:   // 중복된 customId 가 존재하는 경우
+                        message = "이미 존재하는 아이디입니다.";
                         break;
-                    case 403:
-                    case 401:
-                    case 400:
+                    case 403:   // 차단당한 디바이스일 경우
+                        message = callback.GetMessage();
+                        break;
+                    case 401:   // 프로젝트 상태가 '점검'일 경우
+                    case 400:   // 디바이스 정보가 null일 경우
                     default:
                         message = callback.GetMessage();
                         break;
                 }
+
                 if (message.Contains("아이디"))
                 {
                     GuideForIncorretlyEnterData(imageID, message);
@@ -101,3 +116,4 @@ public class RegisterAccount : LoginBase
         });
     }
 }
+
