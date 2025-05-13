b20c03f7af0cd90156341d32c3d49e05213b6557
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-05-13 16:27:35 +0900

    수정 | 버그

--- Assets/00.Main/01.Script/KeyCard/KeyCard_UFO.cs
+++ Assets/00.Main/01.Script/KeyCard/KeyCard_UFO.cs
@@ -66,14 +66,13 @@ public class KeyCard_UFO : MonoBehaviourPunCallbacks, KeyCardEvents
     [PunRPC]
     public void UFOMove(int targetViewID, Vector3 holdPosPosition, Quaternion holdPosRotation)
     {
-
         Transform ufoTr = UFO.transform;
 
         // 시작 위치
         Vector3 startPos = new Vector3(0, 20, -25);
         ufoTr.position = startPos;
 
-        // 타겟 플레이어 GameObject
+        // 타겟 플레이어 찾기
         PhotonView targetPhotonView = PhotonView.Find(targetViewID);
         if (targetPhotonView == null)
         {
@@ -84,19 +83,20 @@ public class KeyCard_UFO : MonoBehaviourPunCallbacks, KeyCardEvents
         GameObject playerGO = targetPhotonView.gameObject;
         Transform playerPos = playerGO.transform;
 
-        // 중간 위치 (플레이어 위), 도착 위치
+        // 이동 경로 설정
         Vector3 midPos = playerPos.position + Vector3.up * 3f;
-        Vector3 endPos = holdPosPosition;
-        Vector3[] path = new[] { midPos, endPos };
+        Vector3 endPos = holdPosPosition +Vector3.up * 4f; ;
+        Vector3[] path = new[] { startPos, midPos, endPos };
 
-        // 이동 트윈
+        // 부드러운 곡선 경로 이동
         Tween moveTween = ufoTr.DOPath(path, flightDuration, PathType.CatmullRom)
             .SetEase(Ease.InOutSine);
 
-        Tween rotateTween = ufoTr.DORotate(new Vector3(0, 360, 0), flightDuration, RotateMode.FastBeyond360)
+        // 회전은 너무 과격하지 않게 시간 늘려서 부드럽게
+        Tween rotateTween = ufoTr.DORotate(new Vector3(0, 360, 0), flightDuration * 2f, RotateMode.FastBeyond360)
             .SetEase(Ease.Linear);
 
-        // 중간 웨이포인트에 도달했을 때 플레이어 비활성화
+        // 중간 위치 도달 시 플레이어 비활성화
         moveTween.OnWaypointChange(index =>
         {
             if (index == 1)
@@ -105,15 +105,20 @@ public class KeyCard_UFO : MonoBehaviourPunCallbacks, KeyCardEvents
             }
         });
 
-        // 최종 도착했을 때 위치 이동 및 재활성화 + UFO 위로 이동
+        // 최종 도착 시 플레이어 위치 이동, 활성화, UFO 상승
         moveTween.OnComplete(() =>
         {
             playerGO.transform.position = endPos;
             playerGO.SetActive(true);
 
+            if (targetPhotonView.IsMine)
+            {
                 var playerScript = targetPhotonView.GetComponent<PlayerControl>();
                 playerScript.ColorChange();
-         
+            }
+      
+
+            // UFO 상승 연출
             ufoTr.DOMoveY(40f, 1f).SetEase(Ease.InOutSine);
         });
     }
