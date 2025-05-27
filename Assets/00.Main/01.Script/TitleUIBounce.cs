60dee6fdb7207a4af6abc7f920fc890f49d21f46
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-05-27 22:57:12 +0900

    수정 | 매칭

--- Assets/00.Main/01.Script/TitleUIBounce.cs
+++ Assets/00.Main/01.Script/TitleUIBounce.cs
@@ -9,6 +9,7 @@ public class TitleUIBounce : MonoBehaviour
     [SerializeField] private float waitTime = 0.5f;            // 대기 시간
     [SerializeField] private float floatScaleMultiplier = 1.1f; // 약간 커지는 효과
     [SerializeField] private float scaleDuration = 1f;         // 스케일 변화 시간
+    [SerializeField] private float scale = 0.9f;         
 
     private Vector3 originalPosition;
     private Vector3 originalScale;
@@ -20,7 +21,7 @@ public class TitleUIBounce : MonoBehaviour
 
         // 시작 위치: 아래쪽으로 이동
         transform.localPosition = originalPosition - new Vector3(0, startOffsetY, 0);
-        transform.localScale = originalScale * 0.9f; // 약간 작게 시작
+        transform.localScale = originalScale * scale; // 약간 작게 시작
 
         Invoke(nameof(StartFloatUp), waitTime);
     }
